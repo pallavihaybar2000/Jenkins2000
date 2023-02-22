12f12cef5eb255acd7f6581b56f8ded3eae84f94
Pallavi <pallavidhaybar2000@gmail.com>
2023-02-22 22:26:55 +0530

    Creating JobChain01 and JobChain02 file

--- JobChain01.java
+++ JobChain01.java
@@ -3,7 +3,7 @@
 public class JobChain01 {
 
 	public static void main(String[] args) {
-System.out.println("Job Chain 01 Welcome ");
+System.out.println("Job Chain 01 Welcome into Jenkins");
 	}
 
 }
--- JobChain02.java
+++ JobChain02.java
@@ -1,6 +1,6 @@
 //package Jenkins;
 
-public class JonChain02 {
+public class JobChain02 {
 
 	public static void main(String[] args) {
 System.out.println("Job Chain02 Welcome into Jenkins");
