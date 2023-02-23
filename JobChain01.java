920cd81ca4d69898c2927ff0b39c628af34a1a5c
Pallavi <pallavidhaybar2000@gmail.com>
2023-02-23 07:49:09 +0530

    Creating Remote Job file

--- JobChain01.java
+++ JobChain01.java
@@ -1,9 +1,9 @@
 //package Jenkins;
 
-public class JobChain01 {
-
-	public static void main(String[] args) {
-System.out.println("Job Chain 01 Welcome into Jenkins");
+public class JobChain01 
+{
+	public static void main(String[] args) 
+	{
+      System.out.println("Job Chain 01 Welcome into Jenkins");
 	}
-
 }
--- JobChain02.java
+++ JobChain02.java
@@ -1,9 +1,9 @@
 //package Jenkins;
 
-public class JobChain02 {
-
-	public static void main(String[] args) {
-System.out.println("Job Chain02 Welcome into Jenkins");
+public class JobChain02
+ {
+	public static void main(String[] args) 
+        {
+          System.out.println("Job Chain02 Welcome into Jenkins");
 	}
-
 }
